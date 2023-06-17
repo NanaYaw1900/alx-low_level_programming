7366dd9a4c8b4abf789f4dfc6e0bde9791890392
foster acquah <fosteracquah64@gmail.com>
2023-06-17 10:12:51 -0700

    Hello, World

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#!/bin/bash
+#include <stdio.h>
+/**
+ * main - Entry piont
+ * 
+ * Return: Always 0 (Succsess)
+ */
+int main(void)
+{
+	print("Size of char: %d byte(s)\N", sizeof(char));
+	printf("Size of an int: %d byte(s)\n", sizeof(int));
+	printf("Size of a long int: %d byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %d byte(s)\n", sizeof(float));
+	return (0);
+}
