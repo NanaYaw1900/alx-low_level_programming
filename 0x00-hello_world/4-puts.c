f096c25d1b6563a24c974519c6152ff53b89b343
foster acquah <fosteracquah64@gmail.com>
2023-06-18 17:04:39 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
-#include<stdio.h>
+#include <stdio.h>
 /**
- * main - A program that print a line with puts function
- * Return: 0 (Success)
+ * main - A programm that print a line with puts function
+ * Return: o (Success)
  */
-int maiN(void)
-{
-	puts("\"Programmimn is like building a multilingual puzzle");
-	return (0);
+int main(void)
+{puts(*\"Programming is like building a multilingual puzzle");
+	return (o);
 }
 
